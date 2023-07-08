08d73d05bfbf18edf4c517526df18659b64547f6
Youmna_Elmezayen <68197709+Youmna-Elmezayen@users.noreply.github.com>
2023-07-08 20:59:40 +0300

    Add files via upload

---
+++ Sheet2Loops_V.cpp
@@ -0,0 +1,16 @@
+    #include <iostream>
+     
+    using namespace std;
+     
+    int main()
+    {
+        int N;
+        cin>>N;
+        int j = 1;
+        for(int i = 0; i < N; i++)
+        {
+            cout<<j<<" "<<j+1<<" "<<j+2<<" "<<"PUM"<<endl;
+            j += 4;
+        }
+        return 0;
+    }
