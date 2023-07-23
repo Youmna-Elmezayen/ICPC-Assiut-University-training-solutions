1b31e2e4626aec0e234a10ab444435e922bef5dd
Youmna_Elmezayen <68197709+Youmna-Elmezayen@users.noreply.github.com>
2023-07-23 16:53:46 +0300

    Add files via upload

---
+++ Sheet6Math_E.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    long long N;
+    cin >> N;
+	
+    for(long long i=1; ; i++)
+    {
+        if(i > N)
+        {
+            cout << i-1 << endl;
+            return 0;
+        }
+        else
+        {
+            N -= i;
+        }
+    }
+}
